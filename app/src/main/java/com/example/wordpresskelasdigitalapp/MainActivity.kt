d48840f165f953642461713dcc79ab36b6adf1c7
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-02-14 15:23:48 +0700

    feat: [webview app] add layout, permission, and webview configuration

--- app/src/main/java/com/example/wordpresskelasdigitalapp/MainActivity.kt
+++ app/src/main/java/com/example/wordpresskelasdigitalapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.wordpresskelasdigitalapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        webView = findViewById(R.id.wv_main)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://kursusdigital.id/kelas/wordpress/")
+
+        val webSettings = webView.settings
+
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
         }
     }
 }
